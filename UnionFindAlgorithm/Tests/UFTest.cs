34adfa86b8e084f98d6049f5e366c424b4c69a03
konstantin <brodin.konstantin@gmail.com>
2015-04-17 15:51:51 +0300

    UFEager implementation and test added

---
+++ UnionFindAlgorithm/Tests/UFTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UnionFindAlgorithm.UFAlgorithms;
+
+namespace UnionFindAlgorithm.Tests
+{
+    public class UfTest
+    {
+        [Test]
+        public void CheckConnected()
+        {
+            UfEager ufEager = new UfEager(10);
+            Assert.IsFalse(ufEager.Connected(0, 1));
+            ufEager.Union(0,1);
+            Assert.IsTrue(ufEager.Connected(0, 1));
+        }
+    }
+}
---
+++ UnionFindAlgorithm/UFAlgorithms/UFEager.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+
+namespace UnionFindAlgorithm.UFAlgorithms
+{
+    public class UfEager
+    {
+        /// <summary>
+        /// Checks whether two provided points are connected
+        /// </summary>
+        /// <returns>True if elements are connected</returns>
+        public bool Connected(int p, int q)
+        {
+            if (p >= sites.Length || q >= sites.Length)
+                return false;
+
+            return sites[p] == sites[q];
+        }
+
+        /// <summary>
+        /// Makes a connection between first and second element
+        /// </summary>
+        public void Union(int p, int q)
+        {
+            if (p >= sites.Length || q >= sites.Length || Connected(p, q)) return;
+            int pid = sites[p];
+            int qid = sites[q];
+
+            for (int i = 0; i < sites.Length; i++)
+            {
+                if (sites[i] == pid) sites[i] = qid;
+            }
+        }
+
+        public UfEager(int n)
+        {
+            sites = Enumerable.Range(0, n - 1).ToArray();
+        }
+
+        private readonly int[] sites;
+    }
+}
